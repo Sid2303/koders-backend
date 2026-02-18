d9690beb47c65f0957d3605036425d058e5fde7c
unknown <subhashr898@gmail.com>
2026-02-18 18:59:30 +0530

    Database Connected

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-console.log("Hello!");
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+async function connectDatabase() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log("Database Connected");
+  } catch (error) {
+    console.log("Database Connection failed:", error.message);
+    process.exit(1);
+  }
+}
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hello, World!",
+  });
+});
+
+app.post("/login", (req, res) => {
+  const { username } = req.body;
+  res.json({
+    message: username,
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+connectDatabase();
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
